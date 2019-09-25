58fb86fdc0cfbc090092fffc72c8e57dbe103abb
Zuzana Svabova <svabova.zuzana@gmail.com>
2019-09-25 20:35:14 +0800

    Created new component, registered material into project

--- FilmSuggestions/ClientApp/src/app/app.module.ts
+++ FilmSuggestions/ClientApp/src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    FormGroup,
+    FormControl,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
     FormsModule,
+    MatSelectModule,
     RouterModule.forRoot([
-
+      { path: '', component: FormComponent }
     ])
   ],
   providers: [],
---
+++ FilmSuggestions/ClientApp/src/app/form/form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
