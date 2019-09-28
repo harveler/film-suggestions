e7e023d40d26cba6e110ee7b29d56dc71919f41c
Zuzana Svabova <svabova.zuzana@gmail.com>
2019-09-28 18:22:33 +0800

    Updated tests on backend

--- FilmSuggestionsTests/FilmControllerTests.cs
+++ FilmSuggestionsTests/FilmControllerTests.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Web.Http;
-using Microsoft.AspNetCore.Mvc;
-using Xunit;
+using FilmSuggestions.Controllers;
+using FilmSuggestions.Models;
+using FilmSuggestions.Services;
 using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using FilmSuggestions.Models;
-using System.Web;
-using System.Net;
-using FilmSuggestions.Controllers;
-using FilmSuggestions.Services;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
+using Xunit;
 
 namespace FilmSuggestionsTests
 {
@@ -18,16 +16,126 @@ namespace FilmSuggestionsTests
         [Fact]
         public void GenerateFilmSuggestion_ReturnsFilm()
         {
-            // assert
-            var film = new Film();
-            var mockService = new Mock<FilmService>(null);
-            // mockService.Setup(u => u.GenerateFilmSuggestion()).Returns();
+            // arrange
+            var mockService = new Mock<FilmService>();
+            var searchedMovie = CreateSearchedMovie();
+            mockService.Setup(u => u.GenerateFilmSuggestion()).Returns(searchedMovie);
             var controller = new FilmController(mockService.Object);
 
             // act
-            // var result = controller.GenerateFilmSuggestion();
+            var result = controller.GenerateFilmSuggestion();
 
-            // Assert.IsType<Film>(result);
+            // assert
+            Assert.IsType<Film>(result);
+        }
+
+        [Fact]
+        public void GetFilmSuggestionBasedOnGenre_ValidParameters_ReturnsFilm()
+        {
+            // arrange
+            var mockService = new Mock<FilmService>();
+            var searchedMovie = CreateSearchedMovie();
+            var listOfIds = new List<int>
+            {
+                12,
+                35,
+                36
+            };
+            mockService.Setup(u => u.GetFilmSuggestionBasedOnGenre(listOfIds)).Returns(searchedMovie);
+            var controller = new FilmController(mockService.Object);
+
+            // act
+            var result = controller.GetFilmSuggestionBasedOnGenre("12,35,36");
+
+            // assert
+            Assert.IsType<Film>(result);
+        }
+
+        [Fact]
+        public void GetFilmSuggestionBasedOnGenre_InvalidParameters_ReturnsEmptyFilm()
+        {
+            // arrange
+            var mockService = new Mock<FilmService>();
+            var searchedMovie = CreateSearchedMovie();
+            var listOfIds = new List<int>
+            {
+                12,
+                35,
+                36
+            };
+            mockService.Setup(u => u.GetFilmSuggestionBasedOnGenre(listOfIds)).Returns(searchedMovie);
+            var controller = new FilmController(mockService.Object);
+
+            // act
+            var result = controller.GetFilmSuggestionBasedOnGenre("12,45,36");
+
+            // assert
+            Assert.True(result.Id == null);
+            Assert.True(result.Title == null);
+            Assert.True(result.Overview == null);
+            Assert.True(result.Genres == null);
+            Assert.True(result.Year == 0);
+        }
+
+        [Fact]
+        public void GetGenres_ReturnsList()
+        {
+            // arrange
+            var mockService = new Mock<FilmService>();
+            var genres = CreateGenres();
+            mockService.Setup(u => u.GetGenres()).Returns(genres);
+            var controller = new FilmController(mockService.Object);
+
+            // act
+            var result = controller.GetGenres();
+
+            // assert
+            Assert.IsType<List<Genre>>(result);
+            Assert.True(result.Count() == 3);
+            Assert.True(result.First().Id == 12);
+        }
+
+        private static SearchMovie CreateSearchedMovie()
+        {
+            SearchMovie result = new SearchMovie
+            { 
+                Id = 21345,
+                Adult = false,
+                Title = "Rambo",
+                Overview = "Vietnam veteran and drifter John J. Rambo (Sylvester Stallone) wanders into a small Washington town in search of an old friend, but is met with intolerance and brutality by the local sheriff, Will Teasle (Brian Dennehy). When Teasle and his deputies restrain and shave Rambo, he flashes back to his time as a prisoner of war and unleashes his fury on the officers. He narrowly escapes the manhunt, but it will take his former commander (Richard Crenna) to save the hunters from the hunted.",
+                ReleaseDate = new DateTime(1974, 7, 10, 7, 10, 24),
+                GenreIds = new List<int>()
+                {
+                12,
+                35
+                },
+                Video = false,
+            };
+
+            return result;
+        }
+
+        private static List<Genre> CreateGenres()
+        {
+            return new List<Genre>()
+            {
+                new Genre
+                {
+                    Id = 12,
+                        Name = "Drama"
+                },
+                new Genre
+                {
+                    Id = 13,
+                        Name = "Horror"
+                },
+                new Genre
+                {
+                    Id = 14,
+                        Name = "Comedy"
+                }
+            };
         }
     }
+
 }
--- FilmSuggestionsTests/FilmServiceTests.cs
+++ FilmSuggestionsTests/FilmServiceTests.cs
@@ -1,7 +1,94 @@
+using FilmSuggestions.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMDbLib.Client;
+using TMDbLib.Objects.Search;
+using Xunit;
+
 namespace FilmSuggestionsTests
 {
     public class FilmServiceTests
     {
-        
+        private FilmService _service;
+
+        public FilmServiceTests()
+        {
+            TMDbClient client = new TMDbClient("f6164895891c0436b46cf51cb281a28a");
+            _service = new FilmService();
+        }
+
+        [Fact]
+        public void GenerateFilmSuggestion_ReturnsSearchMovie()
+        {
+            // act
+            var movie = _service.GenerateFilmSuggestion();
+            var expectedType = typeof(SearchMovie);
+
+            // assert
+            Assert.NotNull(movie);
+            Assert.IsType(expectedType, movie);
+            Assert.True(movie.Adult == false);
+            Assert.True(movie.Video == false);
+        }
+
+        [Fact]
+        public void GetFilmSuggestionBasedOnGenre_ValidOneParameter_ReturnsSearchMovie()
+        {
+            // arrange
+            List<int> genreIds = new List<int>()
+            {
+                12
+            };
+
+            // act
+            var movie = _service.GetFilmSuggestionBasedOnGenre(genreIds);
+
+            // assert
+            Assert.NotNull(movie);
+            Assert.Contains(12, movie.GenreIds);
+        }
+
+        [Fact]
+        public void GetFilmSuggestionBasedOnGenre_ValidMultipleParameters_ReturnsSearchMovie()
+        {
+            // arrange
+            List<int> genreIds = new List<int>()
+            {
+                12,
+                35,
+                36
+            };
+
+            // act
+            var movie = _service.GetFilmSuggestionBasedOnGenre(genreIds);
+
+            // assert
+            Assert.NotNull(movie);
+            Assert.Contains(12, movie.GenreIds);
+            Assert.Contains(35, movie.GenreIds);
+            Assert.Contains(36, movie.GenreIds);
+        }
+
+        [Fact]
+        public void GetFilmSuggestionBasedOnGenre_InvalidParameters_ThrowsException()
+        {
+            // arrange
+            List<int> genreId = new List<int>()
+            {
+                5
+            };
+
+            var task = Task.Run(() =>
+            {
+                var result = _service.GetFilmSuggestionBasedOnGenre(genreId);
+            });
+
+            // act
+            var exception = Record.ExceptionAsync(async() => await task);
+
+            // assert
+            Assert.True(exception != null);
+            Assert.True(exception.Result.Message == "Try again later.");
+        }
     }
 }
