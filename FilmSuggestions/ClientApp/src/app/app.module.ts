2dd2cdaf1c21231ed83bd8a975b17de46810211e
Zuzana Svabova <svabova.zuzana@gmail.com>
2019-09-28 18:14:50 +0800

    Updated routing path and alphabetized imports

--- FilmSuggestions/ClientApp/src/app/app.module.ts
+++ FilmSuggestions/ClientApp/src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormComponent } from './form/form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -26,7 +25,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     MatSelectModule,
     RouterModule.forRoot([
-      { path: '', component: FormComponent }
+      { path: '**', component: FormComponent }
     ])
   ],
   providers: [],
